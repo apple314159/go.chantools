4577307f05a362edf8c57e1f9b437a4b6c66943a
Maxim Bublis <b@codemonkey.ru>
2013-12-04 23:55:11 +0400

    Documentation fix

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -19,7 +19,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// Package pubsub implements channel Publish-Subscribe pattern.
+// Package pubsub implements Publish-Subscribe pattern for channels.
 package pubsub
 
 import (
